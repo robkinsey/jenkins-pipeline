fda495d88a5d726c3024e100fd33004ab521e40f
rob <rob@uhuru.lan>
2020-04-18 17:01:59 +0100

    Added file for my application

---
+++ src/HeatMyRoom.java
@@ -0,0 +1,8 @@
+/**
+ * Simulates all aspects of using a woodstove.
+ */
+class HeatMyRoom {
+    public static void main(String[] args) {
+        System.out.println("The room is now hot!");
+    }
+}
