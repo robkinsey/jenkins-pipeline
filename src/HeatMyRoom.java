16664e174daad96107c81ef409536e2ec2809192
rob <rob@uhuru.lan>
2020-04-18 17:32:06 +0100

    Added new Maven project

--- src/HeatMyRoom.java
+++
@@ -1,8 +0,0 @@
-/**
- * Simulates all aspects of using a woodstove.
- */
-class HeatMyRoom {
-    public static void main(String[] args) {
-        System.out.println("The room is now hot!");
-    }
-}
---
+++ ws-app/src/main/java/com/robkinsey/app/App.java
@@ -0,0 +1,13 @@
+package com.robkinsey.app;
+
+/**
+ * This application will use a woodstove to heat a room.
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "The room is now toasty." );
+    }
+}
